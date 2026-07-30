e1dc36c79038d4a92b21db87dee4aec392a7c86c
Edddddddddy <983166955@qq.com>
2026-07-30 17:28:20 +0800

    feat(sim): add physical differential-drive robot

--- src/voice_nav_sim/launch/display.launch.py
+++ src/voice_nav_sim/launch/display.launch.py
@@ -1,23 +1,24 @@
 from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
-from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
     xacro_file = PathJoinSubstitution(
         [
-            FindPackageShare("voice_nav_sim"),
-            "urdf",
-            "voice_nav_robot.urdf.xacro",
+            FindPackageShare('voice_nav_sim'),
+            'urdf',
+            'voice_nav_robot.urdf.xacro',
         ]
     )
 
     robot_description = ParameterValue(
         Command(
             [
-                FindExecutable(name="xacro"),
-                " ",
+                FindExecutable(name='xacro'),
+                ' ',
                 xacro_file,
             ]
         ),
@@ -25,22 +26,22 @@ def generate_launch_description():
     )
 
     robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="screen",
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
         parameters=[
-            {"robot_description": robot_description},
+            {'robot_description': robot_description},
         ],
     )
 
     joint_state_publisher = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        name="joint_state_publisher",
-        output="screen",
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
         parameters=[
-            {"robot_description": robot_description},
+            {'robot_description': robot_description},
         ],
     )
 
